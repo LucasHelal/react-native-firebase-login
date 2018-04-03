11ad14848d041e5436e8d18e650f622556ea6765
Lucas Georges Helal <lucasghelal@gmail.com>
2018-04-03 14:25:23 -0300

    app login firebase

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyDyXcV7eLdppgAI6gGQ8t0IYsViXOldSog";
+export const FIREBASE_AUTH_DOMAIN = "react-native-firebase-a043f.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://react-native-firebase-a043f.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "react-native-firebase-a043f";
+export const FIREBASE_STORAGE_BUCKET = "react-native-firebase-a043f.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "1054029171934";
+
+export const FACEBOOK_APP_ID = "2072885469656467";
